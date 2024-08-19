b571f3d512a538d45ef51bef1a2380c485a6928e
Keyur Borad <keyurborad5@gmail.com>
2024-08-19 19:38:36 -0400

    Added Randomly spawning robots
    Spawning turtlebots randomly

--- aws-robomaker-bookstore-world-ros2/launch/multirobot_spawn.launch.py
+++ aws-robomaker-bookstore-world-ros2/launch/multirobot_spawn.launch.py
@@ -21,6 +21,16 @@ def generate_launch_description():
     declare_gui=DeclareLaunchArgument(
             name='gui',
             default_value='false')
+    file_path = os.path.join(launch_file_dir,'tuples.txt')
+    tuples_list = []
+
+    with open(file_path, 'r') as file:
+        for line in file:
+            line = line.strip()
+            tup = eval(line)
+            tuples_list.append(tup)
+    # print("Tuples List:", tuples_list)
+    pos = random.sample(tuples_list, 3)
 
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
     x_pose = LaunchConfiguration('x_pose', default='0.00')
@@ -67,13 +77,13 @@ def generate_launch_description():
     )
 
     
-    static_transform_publisher_node = Node(
-        package='tf2_ros',
-        executable='static_transform_publisher',
-        name='link1_broadcaster',
-        arguments=['0', '0', '0', '0', '0', '0', '1', 'map', 'odom'],
-        output='screen',
-    )
+    # static_transform_publisher_node = Node(
+    #     package='tf2_ros',
+    #     executable='static_transform_publisher',
+    #     name='link1_broadcaster',
+    #     arguments=['0', '0', '0', '0', '0', '0', '1', 'map', 'odom'],
+    #     output='screen',
+    # )
     # Declare arguments
     declare_model_arg = DeclareLaunchArgument(
         'model', default_value=os.environ.get('TURTLEBOT3_MODEL', 'burger'),
@@ -88,19 +98,19 @@ def generate_launch_description():
     declare_first_tb3_arg = DeclareLaunchArgument('first_tb3', default_value='tb3_0')
     declare_second_tb3_arg = DeclareLaunchArgument('second_tb3', default_value='tb3_1')
     declare_third_tb3_arg = DeclareLaunchArgument('third_tb3', default_value='tb3_2')
-    
-    declare_first_tb3_x_pos_arg = DeclareLaunchArgument('first_tb3_x_pos', default_value='0.0')
-    declare_first_tb3_y_pos_arg = DeclareLaunchArgument('first_tb3_y_pos', default_value='0.0')
+
+    declare_first_tb3_x_pos_arg = DeclareLaunchArgument('first_tb3_x_pos', default_value=str(pos[0][0]))
+    declare_first_tb3_y_pos_arg = DeclareLaunchArgument('first_tb3_y_pos', default_value=str(pos[0][1]))
     declare_first_tb3_z_pos_arg = DeclareLaunchArgument('first_tb3_z_pos', default_value='0.01')
     declare_first_tb3_yaw_arg = DeclareLaunchArgument('first_tb3_yaw', default_value='0.0')
 
-    declare_second_tb3_x_pos_arg = DeclareLaunchArgument('second_tb3_x_pos', default_value='0.0')
-    declare_second_tb3_y_pos_arg = DeclareLaunchArgument('second_tb3_y_pos', default_value='-1.0')
+    declare_second_tb3_x_pos_arg = DeclareLaunchArgument('second_tb3_x_pos', default_value=str(pos[1][0]))
+    declare_second_tb3_y_pos_arg = DeclareLaunchArgument('second_tb3_y_pos', default_value=str(pos[1][1]))
     declare_second_tb3_z_pos_arg = DeclareLaunchArgument('second_tb3_z_pos', default_value='0.01')
     declare_second_tb3_yaw_arg = DeclareLaunchArgument('second_tb3_yaw', default_value='0.0')
 
-    declare_third_tb3_x_pos_arg = DeclareLaunchArgument('third_tb3_x_pos', default_value='0.0')
-    declare_third_tb3_y_pos_arg = DeclareLaunchArgument('third_tb3_y_pos', default_value='1.0')
+    declare_third_tb3_x_pos_arg = DeclareLaunchArgument('third_tb3_x_pos', default_value=str(pos[2][0]))
+    declare_third_tb3_y_pos_arg = DeclareLaunchArgument('third_tb3_y_pos', default_value=str(pos[2][1]))
     declare_third_tb3_z_pos_arg = DeclareLaunchArgument('third_tb3_z_pos', default_value='0.01')
     declare_third_tb3_yaw_arg = DeclareLaunchArgument('third_tb3_yaw', default_value='0.0')
     
@@ -122,8 +132,15 @@ def generate_launch_description():
 
 
     # Function to create a group for each turtlebot3
-    def create_tb3_group(namespace, x_pos, y_pos, z_pos, yaw,sdf_path):
+    def create_tb3_group(namespace, x_pos, y_pos, z_pos, yaw,sdf_path,tb):
         return GroupAction([
+            Node(
+                package='tf2_ros',
+                executable='static_transform_publisher',
+                name=f'link1_broadcaster_{tb}',
+                arguments=['0', '0', '0', '0', '0', '0', '1', 'map', f'{tb}/odom'],
+                output='screen',
+            ),
             Node(
                 package='robot_state_publisher',
                 executable='robot_state_publisher',
@@ -131,28 +148,20 @@ def generate_launch_description():
                 output='screen',
                 parameters=[{
                     'robot_description': robot_desc,
-                    # Command([
-                    #     'xacro ', os.path.join(
-                    #         get_package_share_directory('aws_robomaker_bookstore_world'),
-                    #         'urdf', f'turtlebot3_{LaunchConfiguration("model")}.urdf.xacro'
-                    #     )
-                    # ]),
                     'publish_frequency': 50.0,
                     'use_sim_time': use_sim_time,
 
-                    'tf_prefix': namespace
+                    # 'tf_prefix': 'tb3_0'
                 }],
-                # remappings=[
-                #     ('/odom', f'/{namespace}/odom'),
-                #     ('/imu',f'/{namespace}/imu')
-                
-                # ],
+                    remappings=[
+                        ('/tf', 'tf'),
+                        ('/tf_static','tf_static')
+                    
+                    ],  
             ),
             Node(
                 package='gazebo_ros',
                 executable='spawn_entity.py',
-                # # name='spawn_entity_'+ f'{namespace.describe()[14:17]}',
-                # namespace='keyur',
                 arguments=[
                     '-file', sdf_path,
                     '-entity', namespace, 
@@ -160,7 +169,6 @@ def generate_launch_description():
                     '-y', y_pos, 
                     '-z', z_pos, 
                     '-Y', yaw, 
-                    # '-param', 'robot_description'
                 ],
                 # remappings=[
                 #     ('/odom', '/new_odom'),
@@ -177,7 +185,7 @@ def generate_launch_description():
         declare_world,
         gazebo_server,
         gazebo_client,
-        static_transform_publisher_node,
+        # static_transform_publisher_node,
         declare_gui,
         declare_first_tb3_arg,
         declare_second_tb3_arg,
@@ -201,15 +209,15 @@ def generate_launch_description():
                          LaunchConfiguration('first_tb3_x_pos'), 
                          LaunchConfiguration('first_tb3_y_pos'), 
                          LaunchConfiguration('first_tb3_z_pos'), 
-                         LaunchConfiguration('first_tb3_yaw'),sdf_path0),
+                         LaunchConfiguration('first_tb3_yaw'),sdf_path0,'tb3_0'),
         create_tb3_group(LaunchConfiguration('second_tb3'), 
                          LaunchConfiguration('second_tb3_x_pos'), 
                          LaunchConfiguration('second_tb3_y_pos'), 
                          LaunchConfiguration('second_tb3_z_pos'), 
-                         LaunchConfiguration('second_tb3_yaw'),sdf_path1),
+                         LaunchConfiguration('second_tb3_yaw'),sdf_path1,'tb3_1'),
         create_tb3_group(LaunchConfiguration('third_tb3'), 
                          LaunchConfiguration('third_tb3_x_pos'), 
                          LaunchConfiguration('third_tb3_y_pos'), 
                          LaunchConfiguration('third_tb3_z_pos'), 
-                         LaunchConfiguration('third_tb3_yaw'),sdf_path2)
+                         LaunchConfiguration('third_tb3_yaw'),sdf_path2,'tb3_2')
     ])
